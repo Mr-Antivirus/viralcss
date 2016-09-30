dda11cb3c8e821795c004e1a53ba7283b01144f0
Dennis Norton <dennis.norton90@gmail.com>
2016-09-30 18:42:36 +0900

    Added browserSync for easier development.

--- gulpfile.js
+++ gulpfile.js
@@ -9,21 +9,80 @@
 'use strict';
 
 const gulp = require('gulp');
+const chmod = require('gulp-chmod');
 const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const gutil = require('gulp-util');
+const browserSync = require('browser-sync').create();
 
 const sassOptions = {
-    outputStyle: 'nested',
-    version: true
+    outputStyle: 'nested'
 };
 
+const chmodOptions = {
+    owner: {
+        read: true,
+        write: true,
+        execute: true
+    },
+    group: {
+        execute: true
+    },
+    others: {
+        execute: true
+    }
+};
+
+var gulp_src = gulp.src;
+gulp.src = function () {
+    return gulp_src.apply(gulp, arguments)
+        .pipe(plumber(function (error) {
+            // Output an error message
+            gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
+            // emit the end event, to properly end the task
+            this.emit('end');
+        }));
+};
+
+// Compile sass into css
 gulp.task('sass', function () {
-    return gulp.src('./scss/viralcss.scss')
+    return gulp.src('scss/viralcss.scss')
         .pipe(sass(sassOptions).on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(chmod(chmodOptions))
+        .pipe(gulp.dest('dist/css'));
 });
 
+// Watch for scss file changes, then compile
 gulp.task('sass:watch', function () {
-    gulp.watch('./scss/**/*.scss', ['sass']);
+    gulp.watch('scss/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass:watch']);
+// serve everything from the dist folder
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: './examples'
+        }
+    });
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve',  function () {
+    browserSync.init({
+        server: ['examples', 'dist']
+    });
+
+    //gulp.watch("scss/**/*.scss", ['sass:inject']);
+    gulp.watch("examples/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass:inject', function () {
+    return gulp.src("scss/**/*.scss")
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(chmod(chmodOptions))
+        .pipe(gulp.dest("examples/css/*"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
