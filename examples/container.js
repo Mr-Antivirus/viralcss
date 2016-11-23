920dbbbf49286390faab24ed55dc8a63a8e5e97c
Dennis Norton <dennis.norton90@gmail.com>
2016-11-23 09:23:40 +0900

    Trying to get iframe child element

--- examples/container.js
+++ examples/container.js
@@ -1,13 +1,24 @@
-var el = document.getElementById("#grid-based");
+var el = document.getElementById("container-frame").document.getElementById("grid-based");
 
-var column = "";
-
+console.log('run')
 //Outer loop
-for(var i = 1; 1 < 13; i++){
-    el.innerHTML += '<div class="column-' + i + '">1/11</div>'
+for (var i = 12; 1 > 0; i++) {
+    el.innerHTML += '<div class="container">' + innerLoop(i) + '</div>'
 }
+
 
 // Inner loop
-for (var j = 1; j < 13; j++) {
-    
+function innerLoop(outerIndex) {
+    var column = "";
+
+    for (var i = 1; i < 13; i++) {
+        column += '<div class="column-' + i + '">1/' + i + '</div>'
+    }
+
+    return column;
 }
+/*
+    <div class="container">
+        <div class="column-1">1/1</div>
+    </div>
+*/
